674d32c48396db2244d6f4267dd3c8aae50d2e80
agajo <agajo@users.noreply.github.com>
2019-09-03 14:47:35 +0900

    created myapp8 bloc with provider

---
+++ lib/my_app_8.dart
@@ -0,0 +1,106 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rxdart/subjects.dart';
+
+class MyApp8 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('MyApp8 is built');
+    return HogeWidget();
+  }
+}
+
+class HogeWidget extends StatelessWidget {
+  final _HogeBloc _hogeBloc = _HogeBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider<_HogeBloc>(
+      builder: (_) => _hogeBloc,
+      dispose: (_, _HogeBloc bloc) => bloc.dispose(),
+      child: Column(
+        children: <Widget>[
+          WidgetA(),
+          WidgetB(),
+        ],
+      ),
+    );
+  }
+}
+
+// Blocを作成。
+class _HogeBloc {
+  _HogeBloc() {
+    // 初期値は最初に流しておく
+    _counterController.add(_counter);
+    // incrementにnullが投げ入れられたら、_counterを1増やしてStreamに流す
+    _incrementController.stream.listen((_) {
+      _counter++;
+      _counterController.add(_counter);
+    });
+  }
+
+  int _counter = 0;
+  final StreamController<void> _incrementController = StreamController<void>();
+  final BehaviorSubject<int> _counterController = BehaviorSubject<int>();
+  Stream<int> get counter => _counterController.stream;
+  StreamSink<void> get increment => _incrementController.sink;
+
+  void dispose() {
+    _incrementController.close();
+    _counterController.close();
+  }
+}
+
+class WidgetA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetA is built.');
+    return RaisedButton.icon(
+      icon: Icon(Icons.plus_one),
+      label: Text('plus 1'),
+      // Provider経由で、HogeBlocのincrementというSinkにnullを投げ入れる
+      onPressed: () => Provider.of<_HogeBloc>(context).increment.add(null),
+    );
+  }
+}
+
+class WidgetB extends StatelessWidget {
+  final WidgetD _widgetD = WidgetD();
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetB is built');
+    return WidgetC(child: _widgetD);
+  }
+}
+
+class WidgetC extends StatelessWidget {
+  WidgetC({this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetC is built.');
+    return Column(
+      children: <Widget>[
+        StreamBuilder(
+          // Provider経由で受け取っているStreamに応答してリビルド
+          stream: Provider.of<_HogeBloc>(context).counter,
+          builder: (_, AsyncSnapshot snapshot) =>
+              Text(snapshot.data.toString()),
+        ),
+        child,
+      ],
+    );
+  }
+}
+
+class WidgetD extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetD is built.');
+    return Text('WidgetD');
+  }
+}
